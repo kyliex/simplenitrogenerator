fa3b869511654a76d454bcb786a36af582d44cb4
kylie <72231419+kyliex@users.noreply.github.com>
2020-10-12 21:42:42 -0400

    Add files via upload

---
+++ nitro.py
@@ -0,0 +1,24 @@
+import random
+import os
+
+
+def Nitro():
+    code = ''.join(random.choices(randomness + randomnum, k=16))
+    return f'https://discord.gift/{code}'
+
+randomness = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijlkmnopqrstuvwxyz'
+
+randomnum = '123456789'
+
+numbers = input("Enter amount of codes to generate or press enter to generate indefinitely: ")
+
+nitro_file = open("nitro.txt", 'a')
+
+try:
+	for i in range(int(numbers)):
+		print(nitro_file.write(Nitro() + "\n"))
+
+
+except:
+	while True:
+		print(nitro_file.write(Nitro() + "\n"))
